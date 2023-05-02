d8bc70cd5d1d075ae58cd0d9db7b398c87aa4026
kiennt <kienhue98@gmail.com>
2023-05-02 08:32:24 +0700

    initial commit

---
+++ src/main/java/com/example/airbnb/utils/ApiConstant.java
@@ -0,0 +1,27 @@
+package com.example.airbnb.utils;
+
+public class ApiConstant {
+    public static final String USER = "/users";
+    public static final String THONG_SO = "/parameters";
+    public static final String LOP_HOC = "/classes";
+
+    public static final String PERMISSION = "/permissions";
+
+    public static final String CHANGE_PASSWORD = "/change-password";
+
+    public static final String GET_ALL_THONG_SO = "/get-all-thong-so";
+
+    public static final String DETAIL = "/detail";
+
+    public static final String ADD = "/add";
+
+    public static final String GET_ALL = "/all";
+
+    public static final String SEARCH = "/search";
+
+    public static final String UPDATE = "/update";
+
+    public static final String DELETE = "/delete";
+
+
+}
---
+++ src/main/java/com/example/airbnb/utils/ColumnInUserDetail.java
@@ -0,0 +1,31 @@
+package com.example.airbnb.utils;
+
+public class ColumnInUserDetail {
+//    uid, username, tenThanh, tenGoi, gioiTinh, ngaySinh, soDienThoaiCaNhan,
+//    email, ngayRuaToi, nguoiRuaToi, nguoiDoDauRuaToi, ngayThemSuc, nguoiThemSuc,
+//    nguoiDoDauThemSuc, ngayBonMang, tenBo, sdtBo, tenMe, sdtMe, diaChi, giaoXuId, lopId, nganh
+    public static final int COLUMN_UID = 1;
+    public static final int COLUMN_USERNAME = 2;
+    public static final int COLUMN_TENTHANH = 3;
+    public static final int COLUMN_TENGOI = 4;
+    public static final int COLUMN_GIOITINH = 5;
+    public static final int COLUMN_NGAYSINH = 6;
+    public static final int COLUMN_SODIENTHOAICANHAN = 7;
+    public static final int COLUMN_EMAIL = 8;
+    public static final int COLUMN_NGAYRUATOI = 9;
+    public static final int COLUMN_NGUOIRUATOI = 10;
+    public static final int COLUMN_NGUOIDODAURUATOI = 11;
+    public static final int COLUMN_NGAYTHEMSUC = 12;
+    public static final int COLUMN_NGUOITHEMSUC = 13;
+    public static final int COLUMN_NGUOIDODAUTHEMSUC = 14;
+    public static final int COLUMN_NGAYBONMANG = 15;
+    public static final int COLUMN_TENBO = 16;
+    public static final int COLUMN_SDTBO = 17;
+    public static final int COLUMN_TENME = 18;
+    public static final int COLUMN_SDTME = 19;
+    public static final int COLUMN_DIACHI = 20;
+    public static final int COLUMN_GIAOXUID = 21;
+    public static final int COLUMN_LOPID = 22;
+    public static final int COLUMN_NGANH = 23;
+
+}
